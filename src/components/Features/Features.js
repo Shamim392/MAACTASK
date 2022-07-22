dfee52b8e2bf581f5515339ad379fd1523feee09
shamim392 <shaminur392@gmail.com>
2022-07-22 15:53:21 +0600

    feature added

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./Features.module.css";
 import { IoCubeOutline } from "react-icons/io5";
+import circle from "../../assects/image/Ellipse 40.png";
+import phone from "../../assects/image/Central phone 2.png";
+
 const Features = () => {
   return (
     <div className={classes.container}>
@@ -8,11 +11,10 @@ const Features = () => {
       <h1 className={classes.heading}>Make more out of your data</h1>
       <div className={classes.row}>
         <div className={classes.column1}>
-          <div className="classes.main">
+          <div className={classes.main}>
             <div className={classes.iconDiv}>
               <IoCubeOutline className={classes.feIcon} />
             </div>
-
             <div>
               <h4 className={classes.iconText}>Real-time analytics</h4>
               <h5 className={classes.iconTexts}>
@@ -20,20 +22,75 @@ const Features = () => {
                 users work on your.
               </h5>
             </div>
-            {/* <div>
-              <h4 className={classes.iconText}>Real-time analytics</h4>
+          </div>
+          <div className={classes.main}>
+            <div className={classes.iconDiv}>
+              <IoCubeOutline className={classes.feIcon} />
+            </div>
+            <div>
+              <h4 className={classes.iconText}>Intuitive dashboard</h4>
               <h5 className={classes.iconTexts}>
                 See product usage, sign-ins, feature <br /> metrics change as
                 users work on your.
               </h5>
-            </div> */}
+            </div>
+          </div>
+          <div className={classes.main}>
+            <div className={classes.iconDiv}>
+              <IoCubeOutline className={classes.feIcon} />
+            </div>
+            <div>
+              <h4 className={classes.iconText}>Smart suggestions</h4>
+              <h5 className={classes.iconTexts}>
+                See product usage, sign-ins, feature <br /> metrics change as
+                users work on your.
+              </h5>
+            </div>
           </div>
         </div>
         <div className={classes.column2}>
-          <h5>hgello</h5>
+          <div>
+            <img className={classes.circle} src={circle} alt="" />
+            <img className={classes.phoneImg} src={phone} alt="" />
+          </div>
         </div>
         <div className={classes.column3}>
-          <h3>hgello</h3>
+          <div className={classes.main3}>
+            <div className={classes.iconDiv3}>
+              <IoCubeOutline className={classes.feIcon} />
+            </div>
+            <div>
+              <h4 className={classes.iconText3}>Multiple views</h4>
+              <h5 className={classes.iconTexts3}>
+                See product usage, sign-ins, feature <br /> metrics change as
+                users work on your.
+              </h5>
+            </div>
+          </div>
+          <div className={classes.main3}>
+            <div className={classes.iconDiv3}>
+              <IoCubeOutline className={classes.feIcon} />
+            </div>
+            <div>
+              <h4 className={classes.iconText3}>AI-led diagnoses</h4>
+              <h5 className={classes.iconTexts3}>
+                See product usage, sign-ins, feature <br /> metrics change as
+                users work on your.
+              </h5>
+            </div>
+          </div>
+          <div className={classes.main3}>
+            <div className={classes.iconDiv3}>
+              <IoCubeOutline className={classes.feIcon} />
+            </div>
+            <div>
+              <h4 className={classes.iconText3}>Responsive</h4>
+              <h5 className={classes.iconTexts3}>
+                See product usage, sign-ins, feature <br /> metrics change as
+                users work on your.
+              </h5>
+            </div>
+          </div>{" "}
         </div>
       </div>
     </div>
